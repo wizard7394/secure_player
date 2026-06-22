50c5aad71ae7367ec158be5cda98368bfe06fad3
AmirHosein <38527978+wizard7394@users.noreply.github.com>
2026-06-22 03:32:19 +0330

    feat: enhance certificate pinning logic to allow bypass in debug mode

--- lib/core/network/certificate_pinning.dart
+++ lib/core/network/certificate_pinning.dart
@@ -1,10 +1,16 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 
 class AppHttpOverrides extends HttpOverrides {
   @override
   HttpClient createHttpClient(SecurityContext? context) {
     return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) {
+        if (kDebugMode) {
+          return true;
+        }
+
+        return false;
+      };
   }
 }
